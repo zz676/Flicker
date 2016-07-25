ad4a2275810dee99c1d8810f2cbe2513c4216e9a
zhisheng zhou <zz676@nyu.edu>
2016-07-24 20:12:38 -0700

    Heterogenous done!

--- app/src/main/java/com/codepath/flicker/adapters/MovieAdapter.java
+++ app/src/main/java/com/codepath/flicker/adapters/MovieAdapter.java
@@ -37,83 +37,148 @@ public class MovieAdapter extends ArrayAdapter<Movie> {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-
-        final Movie movie = getItem(position);
-        final ViewHolder viewHolder;
-
-        //check the existing view being reused
-        if (convertView == null) {
-            viewHolder = new ViewHolder();
-            LayoutInflater inflater = LayoutInflater.from(getContext());
-            convertView = inflater.inflate(R.layout.item_movie, parent, false);
-            viewHolder.tvDate = (TextView) convertView.findViewById(R.id.tvDate);
-            viewHolder.ivImageView = (ImageView) convertView.findViewById(R.id.ivMovieImage);
-            viewHolder.progressBarPicasso = (ProgressBar) convertView.findViewById(R.id.progressBarPicasso);
-            viewHolder.ivYouTubePlayer = (ImageView) convertView.findViewById(R.id.ivYoutubePlayerImage);
-            viewHolder.ivYouTubePlayer.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    final Intent intent = new Intent(getContext(), YouTubePlayerActivity.class);
-                    intent.putExtra("moive_id", movie.getMovieId());
-                    getContext().startActivity(intent);
-                }
-            });
-            viewHolder.tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
-            viewHolder.rbMovieVotes = (RatingBar) convertView.findViewById(R.id.rbMovieVotes);
-            viewHolder.tvAverageVote = (TextView) convertView.findViewById(R.id.tvAverageVote);
-            convertView.setTag(viewHolder);
-        } else {
-            viewHolder = (ViewHolder) convertView.getTag();
-        }
-
-        //clear out image from convertView
-        viewHolder.ivImageView.setImageResource(0);
-        String imageUrl = "";
-        int orientation = getContext().getResources().getConfiguration().orientation;
-        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
-            imageUrl = movie.getPosterPath();
-            //Picasso.with(getContext()).load(movie.getPosterPath()).into(viewHolder.ivImageView);
-        } else if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            imageUrl = movie.getBackdropPath();
-            //Picasso.with(getContext()).load(movie.getBackdropPath()).into(viewHolder.ivImageView);
-        }
-        viewHolder.ivYouTubePlayer.setVisibility(View.INVISIBLE);
-        viewHolder.progressBarPicasso.setVisibility(View.VISIBLE);
-        Picasso.with(getContext()).load(imageUrl)
-                .into(viewHolder.ivImageView, new com.squareup.picasso.Callback() {
-                    @Override
-                    public void onSuccess() {
-                        if (viewHolder.progressBarPicasso != null) {
-                            viewHolder.progressBarPicasso.setVisibility(View.GONE);
-                            viewHolder.ivYouTubePlayer.setVisibility(View.VISIBLE);
-                        }
-                    }
-
-                    @Override
-                    public void onError() {
-
-                    }
-                });
-
-        viewHolder.tvDate.setText(movie.getReleaseDate());
-        viewHolder.tvTitle.setText(movie.getOriginalTitle());
-        viewHolder.tvAverageVote.setText(movie.getAverageReview() + "/10");
-        Drawable drawable = viewHolder.rbMovieVotes.getProgressDrawable();
-        drawable.setColorFilter(Color.parseColor("#118C4E"), PorterDuff.Mode.SRC_ATOP);
-        viewHolder.rbMovieVotes.setRating(Float.parseFloat(movie.getAverageReview()) / 2);
-        return convertView;
+    public int getViewTypeCount() {
+        return 2;
     }
 
-    private static class ViewHolder {
+    @Override
+    public int getItemViewType(int position) {
+        if (Float.parseFloat(movies.get(position).getAverageReview()) > 5) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        int viewType = this.getItemViewType(position);
+        final Movie movie = movies.get(position);
+        switch (viewType) {
+            case 1:
+                final HighViewHolder highViewHolder;
+                View highConvertView = convertView;
+                if (highConvertView == null) {
+                    LayoutInflater inflater = LayoutInflater.from(getContext());
+                    highConvertView = inflater.inflate(R.layout.item_high_reviews_movie, parent, false);
+                    highViewHolder = new HighViewHolder();
+                    highViewHolder.ivImageView = (ImageView) highConvertView.findViewById(R.id.ivMovieImage);
+                    highViewHolder.progressBarPicasso = (ProgressBar) highConvertView.findViewById(R.id.progressBarPicasso);
+                    highViewHolder.ivYouTubePlayer = (ImageView) highConvertView.findViewById(R.id.ivYoutubePlayerImage);
+                    highViewHolder.ivYouTubePlayer.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            final Intent intent = new Intent(getContext(), YouTubePlayerActivity.class);
+                            intent.putExtra("moive_id", movie.getMovieId());
+                            getContext().startActivity(intent);
+                        }
+                    });
+                    highConvertView.setTag(highViewHolder);
+                } else {
+                    highViewHolder = (HighViewHolder) highConvertView.getTag();
+                }
+
+                highViewHolder.ivYouTubePlayer.setVisibility(View.INVISIBLE);
+                highViewHolder.progressBarPicasso.setVisibility(View.VISIBLE);
+                Picasso.with(getContext()).load(movie.getBackdropPath())
+                        .into(highViewHolder.ivImageView, new com.squareup.picasso.Callback() {
+                            @Override
+                            public void onSuccess() {
+                                if (highViewHolder.progressBarPicasso != null) {
+                                    highViewHolder.progressBarPicasso.setVisibility(View.GONE);
+                                    highViewHolder.ivYouTubePlayer.setVisibility(View.VISIBLE);
+                                }
+                            }
+
+                            @Override
+                            public void onError() {
+
+                            }
+                        });
+
+                return highConvertView;
+            case 0:
+                final LowViewHolder lowViewHolder;
+                View lowConvertView = convertView;
+                String imageUrl = "";
+                if (lowConvertView == null) {
+                    LayoutInflater inflater = LayoutInflater.from(getContext());
+                    lowConvertView = inflater.inflate(R.layout.item_low_reviews_movie, parent, false);
+                    lowViewHolder = new LowViewHolder();
+                    lowViewHolder.tvDate = (TextView) lowConvertView.findViewById(R.id.tvDate);
+                    lowViewHolder.tvTitle = (TextView) lowConvertView.findViewById(R.id.tvTitle);
+                    lowViewHolder.rbMovieVotes = (RatingBar) lowConvertView.findViewById(R.id.rbMovieVotes);
+                    lowViewHolder.tvAverageVote = (TextView) lowConvertView.findViewById(R.id.tvAverageVote);
+                    lowViewHolder.ivImageView = (ImageView) lowConvertView.findViewById(R.id.ivMovieImage);
+                    lowViewHolder.progressBarPicasso = (ProgressBar) lowConvertView.findViewById(R.id.progressBarPicasso);
+                    lowViewHolder.ivYouTubePlayer = (ImageView) lowConvertView.findViewById(R.id.ivYoutubePlayerImage);
+                    lowViewHolder.ivYouTubePlayer.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            final Intent intent = new Intent(getContext(), YouTubePlayerActivity.class);
+                            intent.putExtra("moive_id", movie.getMovieId());
+                            getContext().startActivity(intent);
+                        }
+                    });
+                    lowConvertView.setTag(lowViewHolder);
+                } else {
+                    lowViewHolder = (LowViewHolder) lowConvertView.getTag();
+                }
+
+                lowViewHolder.ivImageView.setImageResource(0);
+                int orientation = getContext().getResources().getConfiguration().orientation;
+                if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+                    imageUrl = movie.getPosterPath();
+                    //Picasso.with(getContext()).load(movie.getPosterPath()).into(viewHolder.ivImageView);
+                } else if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                    imageUrl = movie.getBackdropPath();
+                    //Picasso.with(getContext()).load(movie.getBackdropPath()).into(viewHolder.ivImageView);
+                }
+                lowViewHolder.ivYouTubePlayer.setVisibility(View.INVISIBLE);
+                lowViewHolder.progressBarPicasso.setVisibility(View.VISIBLE);
+                Picasso.with(getContext()).load(imageUrl)
+                        .into(lowViewHolder.ivImageView, new com.squareup.picasso.Callback() {
+                            @Override
+                            public void onSuccess() {
+                                if (lowViewHolder.progressBarPicasso != null) {
+                                    lowViewHolder.progressBarPicasso.setVisibility(View.GONE);
+                                    lowViewHolder.ivYouTubePlayer.setVisibility(View.VISIBLE);
+                                }
+                            }
+
+                            @Override
+                            public void onError() {
+
+                            }
+                        });
+
+                lowViewHolder.tvDate.setText(movie.getReleaseDate());
+                lowViewHolder.tvTitle.setText(movie.getOriginalTitle());
+                lowViewHolder.tvAverageVote.setText(movie.getAverageReview() + "/10");
+                Drawable drawable = lowViewHolder.rbMovieVotes.getProgressDrawable();
+                drawable.setColorFilter(Color.parseColor("#118C4E"), PorterDuff.Mode.SRC_ATOP);
+                lowViewHolder.rbMovieVotes.setRating(Float.parseFloat(movie.getAverageReview()) / 2);
+                return lowConvertView;
+            default:
+                return null;
+        }
+    }
+
+    private static class LowViewHolder {
         ImageView ivImageView;
         ProgressBar progressBarPicasso;
         ImageView ivYouTubePlayer;
         TextView tvTitle;
-        //TextView tvOverView;
         RatingBar rbMovieVotes;
         TextView tvAverageVote;
         TextView tvDate;
+    }
+
+    private static class HighViewHolder {
+        ImageView ivImageView;
+        ProgressBar progressBarPicasso;
+        ImageView ivYouTubePlayer;
     }
 
 }
